8ad5ccf37a6fb935eb2f834257cfe3de0d6faece
Unfinishedgod <shjj08@gmail.com>
2024-02-26 01:23:10 +0900

    add ch10

--- ch_10_u_and_i/lib/screen/home_screen.dart
+++ ch_10_u_and_i/lib/screen/home_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class homeScreen extends StatelessWidget {
-  
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+  }
 }

2d77c138ce47cdec807c9f076c0f6c3a78792be7
Unfinishedgod <shjj08@gmail.com>
2024-02-26 22:25:53 +0900

    ch 10

--- ch_10_u_and_i/lib/screen/home_screen.dart
+++ ch_10_u_and_i/lib/screen/home_screen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime firstDay = DateTime.now();  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,20 +30,48 @@ class HomeScreen extends StatelessWidget {
           // 반대축 최대 크기로 늘리기
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _DDay(),
+            _DDay(
+              // 하트 눌렀을 때 실행할 함수 전달
+              onHeartPressed: onHeartPressed,
+              firstDay: firstDay, // 
+            ),
             _CoupleImage(),
           ],
         ),
       ),
     );
   }
+
+
+
+  // 하트 눌렀을 때 실행할 함수
+  void onHeartPressed() {
+    // 상태 변경 시 setState() 함수 실행
+    setState(() {
+      // firstDay 변수에서 하루 빼기
+      firstDay = firstDay.subtract(Duration(days: 1));
+    });
+  }  
 }
 
+
 class _DDay extends StatelessWidget {
+  // 하트 눌렀을 때 실행할 함수
+  final GestureTapCallback onHeartPressed;
+  final DateTime firstDay;
+
+  _DDay({
+    required this.onHeartPressed, // 상위에서 함수 입력 받기
+    required this.firstDay, // 날짜 변수로 입력 받기
+  });
+
+
   @override
   Widget build(BuildContext context) {
     // 테마 불러오기
     final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now(); // 현재 날짜시간
+
     return Column(
       children: [
         const SizedBox(height: 16.0),
@@ -46,13 +85,16 @@ class _DDay extends StatelessWidget {
           style: textTheme.bodyText1,
         ),
         Text(
-          '2021.11.23',
+          // '2021.11.23',
+          //  DateTima을 년 월 일 형태로 변경
+          '${firstDay.year}.${firstDay.month}.${firstDay.day}',
           style: textTheme.bodyText2,
-        ),
+        ),    
         const SizedBox(height: 16.0),
         IconButton(
           iconSize: 60.0,
-          onPressed: () {},
+          // onPressed: () {},
+          onPressed: onHeartPressed, // 아이콘 눌렀을 때 실행할 함수
           icon: Icon(
             Icons.favorite,
             color: Colors.red,
@@ -60,7 +102,10 @@ class _DDay extends StatelessWidget {
         ),
         const SizedBox(height: 16.0),
         Text(
-          'D+365',
+          // 'D+365',
+          // DDay 계산
+          'D+${DateTime(now.year, now.month, 
+                now.day).difference(firstDay).inDays + 1}',
           style: textTheme.headline2,
         ),
       ],

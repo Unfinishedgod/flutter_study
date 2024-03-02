89f653772eae825305c8b9ad187220d6f9a20b00
Unfinishedgod <shjj08@gmail.com>
2024-03-02 18:27:41 +0900

    ch 17 start schedule

---
+++ ch_17_calendar_scheduler_desktop/lib/component/schedule_card.dart
@@ -0,0 +1,2 @@
+import 'package:ch_17_calendar_scheduler_desktop/const/colors.dart';
+import 'package:flutter/material.dart';
--- ch_17_calendar_scheduler_desktop/lib/screen/home_screen.dart
+++ ch_17_calendar_scheduler_desktop/lib/screen/home_screen.dart
@@ -1,8 +1,25 @@
 import 'package:ch_17_calendar_scheduler_desktop/component/main_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  //  StatelessWidget 에서 StatefulWidget으로 전환
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+// }
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +28,25 @@ class HomeScreen extends StatelessWidget {
         child: Column( // 달력과 리스트 세로로 배치
           children: [
             // 미리 작업해둔 달력 위젯 보여주기
-            MainCalendar(),
+            MainCalendar(
+              selectedDate: selectedDate, // 선택된 날짜 전달
+
+              // 날짜가 선택됐을 때 실행할 함수
+              onDaySelected: onDaySelected,
+            ),
           ],
-        )
-      )
+        ),
+      ),
       // body: Text('Home Screen'),
     );
   }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    //  날짜 선택될 때마다 실행할 함수
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+
 }
+

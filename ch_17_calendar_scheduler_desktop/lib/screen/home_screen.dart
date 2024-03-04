74f950ddee4d5ed700bdc4d6436b72df21ef5b7c
Unfinishedgod <shjj08@gmail.com>
2024-03-04 23:50:05 +0900

    ch 18 end

--- ch_17_calendar_scheduler_desktop/lib/screen/home_screen.dart
+++ ch_17_calendar_scheduler_desktop/lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:ch_17_calendar_scheduler_desktop/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -10,7 +12,7 @@ import 'package:ch_17_calendar_scheduler_desktop/component/today_banner.dart';
 
 
 class HomeScreen extends StatefulWidget {
-  //  StatelessWidget 에서 StatefulWidget으로 전환
+  // ➊ StatelessWidget에서 StatefulWidget으로 전환
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -19,70 +21,106 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   DateTime selectedDate = DateTime.utc(
+    // ➋ 선택된 날짜를 관리할 변수
     DateTime.now().year,
     DateTime.now().month,
     DateTime.now().day,
   );
-// }
-
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton( // 새 일정 버튼
+      floatingActionButton: FloatingActionButton(
+        // ➊ 새 일정 버튼
         backgroundColor: PRIMARY_COLOR,
         onPressed: () {
-          showModalBottomSheet( // BottomSheet 열기
+          showModalBottomSheet(
+            // ➋ BottomSheet 열기
             context: context,
-            isDismissible: true, // 배경 탭했을 때 bottomsheet 닫기
-            builder: (_) => ScheduleBottomSheet(
-              selectedDate: selectedDate, // 선택된 날짜 (selectedDate) 넘겨 주기
-            ), 
-            // BottomSheet의 높이를 화면의 최대 높이로
-            // 정의하고 스크롤 가능하게 변경
+            isDismissible: true, // ➌ 배경 탭했을 때 BottomSheet 닫기
             isScrollControlled: true,
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate, // 선택된 날짜 (selectedDate) 넘겨주기
+            ),
           );
         },
         child: Icon(
           Icons.add,
         ),
       ),
-      body: SafeArea( // 시스템 UI 피해서 UI 구현
-        child: Column( // 달력과 리스트 세로로 배치
+      body: SafeArea(
+        // 시스템 UI 피해서 UI 구현하기
+        child: Column(
+          // 달력과 리스트를 세로로 배치
           children: [
-            // 미리 작업해둔 달력 위젯 보여주기
             MainCalendar(
-              selectedDate: selectedDate, // 선택된 날짜 전달
+              selectedDate: selectedDate, // 선택된 날짜 전달하기
 
               // 날짜가 선택됐을 때 실행할 함수
               onDaySelected: onDaySelected,
             ),
             SizedBox(height: 8.0),
-            TodayBanner(
-              selectedDate: selectedDate, 
-              count: 0
+            StreamBuilder<List<Schedule>>(
+                stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                builder: (context, snapshot) {
+                  return TodayBanner(
+                    selectedDate: selectedDate,
+                    count: snapshot.data?.length ?? 0,
+                  );
+                }
             ),
             SizedBox(height: 8.0),
-            ScheduleCard(
-              startTime: 13,
-              endTime:14,
-              content: '코딩 공부하기',
+            Expanded(
+              // ➊ 남는 공간을 모두 차지하기
+              child: StreamBuilder<List<Schedule>>(
+                // ➋ 일정 정보가 Stream으로 제공되기 때문에 StreamBuilder 사용
+                stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    // ➌ 데이터가 없을 때
+                    return Container();
+                  }
+
+                  return ListView.builder(
+                    // ➍ 화면에 보이는 값들만 렌더링하는 리스트
+                    itemCount: snapshot.data!.length, // ➎ 리스트에 입력할 값들의 총 개수
+                    itemBuilder: (context, index) {
+                      final schedule =
+                          snapshot.data![index]; // ➏ 현재 index에 해당되는 일정
+                      return Dismissible(
+                        key: ObjectKey(schedule.id),
+                        // ➊ 유니크한 키값
+                        direction: DismissDirection.startToEnd,
+                        // ➋ 밀기 방향 (오른쪽에서 왼쪽으로)
+                        onDismissed: (DismissDirection direction) {
+                          // ➌ 밀기 했을 때 실행할 함수
+                          GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              bottom: 8.0, left: 8.0, right: 8.0),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      // body: Text('Home Screen'),
     );
   }
 
   void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
-    //  날짜 선택될 때마다 실행할 함수
+    // ➌ 날짜 선택될 때마다 실행할 함수
     setState(() {
       this.selectedDate = selectedDate;
     });
   }
-
 }
-

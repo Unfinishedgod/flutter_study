a7b415cbe1b7355f3c46ec34d87ce1481cc80f6c
Unfinishedgod <shjj08@gmail.com>
2024-03-12 01:27:17 +0900

    modify schedule repository

--- ch_17_calendar_scheduler_desktop/lib/repository/schedule_repository.dart
+++ ch_17_calendar_scheduler_desktop/lib/repository/schedule_repository.dart
@@ -5,6 +5,9 @@ import 'package:ch_17_calendar_scheduler_desktop/model/schedule_model.dart';
 import 'package:dio/dio.dart';
 
 class ScheduleRepository {
+
+
+
   final _dio = Dio();
   // 안드로이드에서는 10.0.2.2가 localhost에 해당함
   final _targetUrl = 'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/schedule';
@@ -27,4 +30,25 @@ class ScheduleRepository {
         )
             .toList();
   } 
+
+  
+  Future<String> createSchedule({
+    required ScheduleModel schedule, 
+  }) async {
+    final json = schedule.toJson(); // JSON으로 변환
+
+    final resp = await _dio.post(_targetUrl, data: json);
+
+    return resp.data?['id'];
+  }
+
+  Future<String> deleteSchedule({
+    required String id,
+  }) async {
+    final resp = await _dio.delete(_targetUrl, data: {
+      'id': id, // 삭제할 id값
+    });
+
+    return resp.data?['id']; // 삭제된 ID값 변환
+  }
 }

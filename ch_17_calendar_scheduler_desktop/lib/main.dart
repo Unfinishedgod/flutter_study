ec662154425c6a1c91f4692ab7531c65ca212357
Unfinishedgod <shjj08@gmail.com>
2024-03-15 20:29:39 +0900

    provider

--- ch_17_calendar_scheduler_desktop/lib/main.dart
+++ ch_17_calendar_scheduler_desktop/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:ch_17_calendar_scheduler_desktop/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
+import 'package:ch_17_calendar_scheduler_desktop/provider/schedule_provider.dart';
+import 'package:ch_17_calendar_scheduler_desktop/repository/schedule_repository.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   //  플러터 프레임워크가 준비될 때까지 대기 
@@ -16,9 +19,15 @@ void main() async {
   // GetIt에 데이터베이스 변수 주입하기
   GetIt.I.registerSingleton<LocalDatabase>(database);
 
+  final repository = ScheduleRepository();
+  final scheduleProvider = ScheduleProvider(repository: repository);
+
   runApp(
-    MaterialApp(
-      home: HomeScreen(),
+    ChangeNotifierProvider( // provider 하위 위젯에 제공하기
+      create: (_) => scheduleProvider,
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
     ),
-  );
+  )
 }
--- ch_17_calendar_scheduler_desktop/lib/provider/schedule_provider.dart
+++ ch_17_calendar_scheduler_desktop/lib/provider/schedule_provider.dart
@@ -14,10 +14,11 @@ class ScheduleProvider extends ChangeNotifier {
   );
   Map<DateTime, List<ScheduleModel>> cache = {}; // 일정 정보를 저장해둘 변수
 
-  ScheduleProvixer({
+  ScheduleProvider({
     required this.repository,
   }) : super() {
     getSchedules(date: selectedDate);
+  }
 
   void getSchedules({
     required DateTime date, 
@@ -30,5 +31,51 @@ class ScheduleProvider extends ChangeNotifier {
     notifyListeners(); // 리슨하는 위젯들 업데이트
   }
 
+  void createSchedule({
+    required ScheduleModel schedule,
+  }) async {
+    final targetDate = schedule.date;
+
+    final savedSchedule = await repository.createSchedule(schedule: schedule);
+
+    cache.update(
+      targetDate,
+          (value) => [ // 현존하는 캐시 리스트 끝에 새로운 일정 추가
+        ...value,
+        schedule.copyWith(
+          id: savedSchedule,
+        ),
+      ]..sort(
+          (a,b) => a.startTime.compareTo(
+        b.startTime,  
+        ),
+      ),
+      // 날짜에 해당되는 값이 없다면  새로운 리스트에 새로운 일정 하나만 추가
+      ifAbsent: () => [schedule],
+    );
+
+    notifyListeners();
+  }
+
+  void deleteSchedule({
+    required DateTime date,
+    required String id,
+  }) async {
+    final resp = await repository.deleteSchedule(id: id);
+
+    cache.update( // 캐시에서 데이터 삭제
+      date,
+          (value) => value.where((e) => e.id != id).toList(),
+      ifAbsent: () => [],
+    );
+
+    notifyListeners();
+  }
+
+  void changeSelectedDate({
+    required DateTime date,
+  }) {
+    selectedDate = date; // 현재 선택된 날짜를 매개변수로 입력받은 날짜로 변경
+    notifyListeners();
   }
 }
